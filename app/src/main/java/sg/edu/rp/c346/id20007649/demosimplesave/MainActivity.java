1e4bff765e9fac16206a56c9992f6fac494a98ba
20007649 <20007649@rp.edu.sg>
2021-07-09 15:09:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20007649/demosimplesave/MainActivity.java
@@ -0,0 +1,45 @@
+package sg.edu.rp.c346.id20007649.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+       SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+       String message = prefs.getString("greetings", "No greetings name");
+
+       Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+        SharedPreferences.Editor prefEdit = prefs.edit();
+
+        prefEdit.putString("greetings", "Hello");
+
+        prefEdit.commit();
+    }
+}
